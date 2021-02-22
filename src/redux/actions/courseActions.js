316ef8b953cfbda4bd2d8b6e09657e157b22cf7a
Francesc <fpbosch@gmail.com>
2021-02-22 08:04:36 +0100

    Solventat problema de les accions

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -5,12 +5,12 @@ export function loadCourseSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
-export function createCourseSuccess(courses) {
-  return { type: types.CREATE_COURSE_SUCCESS, courses };
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
-export function updateCourseSuccess(courses) {
-  return { type: types.UPDATE_COURSE_SUCCESS, courses };
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
@@ -25,10 +25,9 @@ export function loadCourses() {
 
 export function saveCourse(course) {
   return function(dispatch, getState) {
-    debugger;
     return courseApi.saveCourse(course).then(savedCourse=> {
-      course.id ? dispatch(updateCourseSuccess(course)) :
-                  dispatch(createCourseSuccess(course))
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+                  dispatch(createCourseSuccess(savedCourse))
     }).catch(error=> {
       throw error;
     });
